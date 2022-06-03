1fbdf9d11e44a09bc16fbcc43432b72e16ba602d
Cristian Ponce <cristian.ponce@onephase.com>
2022-06-03 13:55:29 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// import inmutability from './inmutability';
+import bigTestCode from './bigTestCode';
+
+function smallTestCode(){
+  console.log('Do something small');
+}
+
+function main() {
+  console.log('---------- Cool Programing ----------\n');
+  smallTestCode();
+  bigTestCode();
+}
+
+main();
